e4c85d7cc394ee224226603141ae82b191b42eb6
Walker Henderson <wjahenderson@gmail.com>
2025-06-11 22:35:42 -0700

    Start on visualization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { useState, useCallback, useEffect } from "react";
-import { LufsMeter } from "@/processing/LufsMeter";
+import { LufsMeter, LufsMeterResult } from "@/processing/LufsMeter";
+import WavesurferPlayer from "@wavesurfer/react";
+import { LineChart } from "@mui/x-charts/LineChart";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,6 +18,7 @@ function useAudioDrop(
   onAudioDecoded: (result: {
     buffers: Float32Array[];
     sampleRate: number;
+    blob: Blob;
   }) => void
 ) {
   const [dragActive, setDragActive] = useState(false);
@@ -29,6 +32,8 @@ function useAudioDrop(
 
       const arrayBuffer = await file.arrayBuffer();
       const audioCtx = new window.AudioContext();
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
       try {
         const decoded = await audioCtx.decodeAudioData(arrayBuffer);
         // Use the first channel for simplicity
@@ -39,6 +44,7 @@ function useAudioDrop(
         onAudioDecoded({
           buffers: floatArrays,
           sampleRate: decoded.sampleRate,
+          blob: file,
         });
       } catch {
         // Handle decode error
@@ -63,26 +69,110 @@ function useAudioDrop(
 }
 
 export default function Home() {
+  const [wavesurfer, setWavesurfer] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const onReady = (ws) => {
+    console.log("ready!");
+    setWavesurfer(ws);
+    setIsPlaying(false);
+  };
+
+  const onPlayPause = () => {
+    if (wavesurfer) {
+      wavesurfer.playPause();
+    }
+  };
+
   const [audioData, setAudioData] = useState<{
     buffers: Float32Array[];
+    lufsData: LufsMeterResult;
     sampleRate: number;
+    blob: Blob;
   } | null>(null);
   const { dragActive, handleDrop, handleDragOver, handleDragLeave } =
-    useAudioDrop(setAudioData);
+    useAudioDrop((audioData) => {
+      setAudioData({
+        ...audioData,
+        lufsData: new LufsMeter(audioData.sampleRate).measure(
+          audioData.buffers
+        ),
+      });
+
+      wavesurfer.loadBlob(audioData?.blob);
+    });
 
   useEffect(() => {
-    if (!audioData) {
-      return;
-    }
-
-    const lufsMeter = new LufsMeter(audioData.sampleRate);
-    console.log(lufsMeter.measure(audioData.buffers));
+    console.log(audioData);
   });
   return (
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div style={{ width: 500 }}>
+          <WavesurferPlayer
+            height={100}
+            waveColor="#4F4A85"
+            progressColor="#383351"
+            url="/empty.wav"
+            onReady={onReady}
+            onError={(e) => console.error("Wavesurfer error:", e)}
+            onPlay={() => setIsPlaying(true)}
+            onPause={() => setIsPlaying(false)}
+          />
+        </div>
+
+        <button onClick={onPlayPause}>{isPlaying ? "Pause" : "Play"}</button>
+        {audioData && (
+          <>
+            <LineChart
+              series={[
+                {
+                  label: "Momentary loudness",
+                  data: audioData.lufsData.blockLoudness,
+                  showMark: false,
+                },
+                {
+                  label: "Relative gate threshold",
+                  data: audioData.lufsData.blockLoudness.map(
+                    () => audioData.lufsData.relativeThreshold
+                  ),
+                  showMark: false,
+                },
+              ]}
+              xAxis={[
+                {
+                  data: Array.from(
+                    audioData.lufsData.blockLoudness.map((_, i) => i * 0.1)
+                  ),
+                  valueFormatter: (v: number) => {
+                    const minutes = Math.floor(v / 60);
+                    const seconds = (v % 60).toFixed(0).padStart(2, "0");
+                    return `${minutes}:${seconds}`;
+                  },
+                  // scaleType: "time",
+                },
+              ]}
+              yAxis={[
+                {
+                  min: -35,
+                  max: 0,
+                  colorMap: {
+                    type: "piecewise",
+                    colors: ["#444", "#444 ", "#4254FB"],
+                    thresholds: [
+                      -1000,
+                      audioData.lufsData.relativeThreshold,
+                      0,
+                    ],
+                  },
+                },
+              ]}
+              height={500}
+              width={500}
+            />
+          </>
+        )}
         <div
           onDrop={handleDrop}
           onDragOver={handleDragOver}
